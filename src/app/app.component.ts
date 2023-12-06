eba6235f569fd8b68fafe9a5947c8d00b0dc6ae8
Ameer Hamza <ameer.hamza928942@gmail.com>
2023-12-06 22:30:35 +0500

    Integration of NgPrime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { InfoModule } from './information/info/info.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, HeaderComponent, SidebarComponent, InfoModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learningApp';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { SalesComponent } from './sales/sales.component';
+import { InfoComponent } from './information/info/info.component';
+import { AgendaDeatilsComponent } from './information/info/components/agenda-deatils/agenda-deatils.component';
+import { AttendenceDeatilsComponent } from './information/info/components/attendence-deatils/attendence-deatils.component';
+import { SalesDetailsComponent } from './information/info/components/sales-details/sales-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: CardComponent
+  },
+  {
+    path: 'sales',
+    component: SalesComponent
+  },
+  {
+    path: 'info',
+    component: InfoComponent,children:[
+      {path:'agenda-deatils',component:AgendaDeatilsComponent},
+      {path:'attendence-details',component:AttendenceDeatilsComponent},
+      {path:'sales-details',component:SalesDetailsComponent},
+    ]
+  },
+];
